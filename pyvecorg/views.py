9a179a426e76cc5f202a61d5baf1bb1069c92e3c
Honza Javorek <mail@honzajavorek.cz>
2018-08-08 22:28:55 +0200

    add dead simple API

--- pyvecorg/views.py
+++ pyvecorg/views.py
@@ -2,20 +2,13 @@ import os
 from datetime import datetime
 
 from flask import (request, render_template, redirect, url_for,
-                   send_from_directory)
+                   send_from_directory, jsonify)
 
 from pyvecorg import app
 from pyvecorg.data import load_data, select_language
 
 
 data = load_data()
-
-
-@app.route('/')
-def index_redirect():
-    if request.accept_languages.best_match(['en', 'cs', 'sk']) == 'en':
-        return redirect(url_for('index', lang='en'))
-    return redirect(url_for('index', lang='cs'))
 
 
 @app.route('/favicon.ico')
@@ -25,9 +18,30 @@ def favicon():
                                mimetype='image/vnd.microsoft.icon')
 
 
+@app.route('/')
+def index_redirect():
+    return redirect(url_for('index', lang=detect_lang(request)))
+
+
 @app.route('/<lang>/')
 def index(lang):
     context = select_language(data, lang)
     context['lang'] = lang
     context['now'] = datetime.now()
     return render_template('index.html', **context)
+
+
+@app.route('/api.json')
+def api_redirect():
+    return redirect(url_for('api', lang=detect_lang(request)))
+
+
+@app.route('/<lang>/api.json')
+def api(lang):
+    return jsonify(select_language(data, lang))
+
+
+def detect_lang(request):
+    if request.accept_languages.best_match(['en', 'cs', 'sk']) == 'en':
+        return 'en'
+    return 'cs'
