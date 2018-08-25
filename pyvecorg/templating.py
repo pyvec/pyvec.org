e4adb26b7cd63087ed5884fd8055dc955c541788
Honza Javorek <mail@honzajavorek.cz>
2018-08-25 19:06:16 +0200

    have avatars as local files

--- pyvecorg/templating.py
+++ pyvecorg/templating.py
@@ -1,12 +1,11 @@
 import re
-import os
-import hashlib
 import textwrap
-from urllib.parse import quote
 
 import jinja2
 import requests
+from slugify import slugify
 from markdown import markdown
+from flask import url_for
 
 from pyvecorg import app
 
@@ -20,29 +19,7 @@ def convert_markdown(text):
 
 @app.template_filter('avatar_url')
 def get_avatar_url(member):
-    no_avatar = 'https://www.gravatar.com/avatar/0000?d=mm&f=y'
-
-    try:
-        key = member['avatar']
-        value = member[key]
-    except KeyError:
-        return no_avatar
-
-    if key == 'email':
-        email_hash = hashlib.md5(value.lower().strip().encode()).hexdigest()
-        return f'https://www.gravatar.com/avatar/{email_hash}?size=400&d=404'
-    elif key == 'twitter':
-        if os.getenv('DISABLE_TWITTER_AVATARS'):
-            return no_avatar
-        username = quote(value)
-        url = f'https://twitter.com/{username}/profile_image'
-        url = requests.head(url).headers.get('location')
-        return url.replace('_normal', '_400x400')
-    if key == 'github':
-        username = quote(value)
-        return f'https://github.com/{username}.png'
-
-    return no_avatar
+    return url_for('avatar', name=slugify(member['name']))
 
 
 @app.template_filter('url')
